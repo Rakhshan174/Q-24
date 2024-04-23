8d8094e26f6d9728d9659ef7222898fbd120cfc3
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:12:36 -0700

    Q 24 TS/JS for 45 assignment

---
+++ main.ts
@@ -0,0 +1,76 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more 
+// comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests for equality and inequality with strings
+
+// • Tests using the lower case function
+
+// • Numerical tests involving equality and inequality, greater than and 
+// less than, greater than or equal to, and less than or equal to
+
+// • Tests using "and" and "or" operators
+
+// • Test whether an item is in a array
+
+// • Test whether an item is not in a array
+
+let name_1 : string = "Muhammad";
+let name_2 : string = " Muhammad Ibrahim";
+let name_3 : string = "Muhammad Ibrahim Khan";
+
+// if(name_1 == name_3){
+//     console.log("name are equal");
+// }else{
+//     console.log("name are note equal");
+// }
+
+// if(name_1 != name_2){
+//     console.log("name are equal");
+// }
+ 
+// if(name_1 != name_3){
+//     console.log("name are equal");
+// }
+
+
+let age_1 : number = 18
+let age_2 : number = 22
+
+//  if(age_1 == 18){
+//     console.log("eligible for vote")
+//  }
+
+
+//  if(age_1 != 22){
+//     console.log("eligible for vote in older category")
+//  }
+
+
+
+// if(age_1 <= age_2){ // less
+//     console.log("Younger")
+// }
+
+
+// if(age_2 >= age_1){// greater
+//     console.log("Older")
+// }
+
+
+// if(age_1 == 18 && age_2 == 22){
+//     console.log("person is eligible for vote")
+// }
+
+// if(age_1 == 18 || age_2 != 22){
+//     console.log("person is eligible not for vote")
+// }
+
+let country : String [] = ["Pakistan", "India", "Japan","China"]
+
+// if(country.includes("Pakistan")){
+//     console.log("Pakistan is in country list")
+// }
+
+if(!country .includes("America")){
+    console.log("America is not include in an array")
+}
+
